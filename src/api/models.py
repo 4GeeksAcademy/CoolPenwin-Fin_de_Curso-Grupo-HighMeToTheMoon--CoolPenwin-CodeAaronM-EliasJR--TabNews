6f9ef3769ed56f048ef9b08228482b24e83593f5
Alberto <coolPenwin@proton.me>
2024-09-28 17:18:07 +0000

    primer punto salvado

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,25 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+    
+class Newspaper(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    logo = db.Column(db.String(255))
+    link = db.Column(db.String(255))
+
+    # articles = db.relationship('Article', back_populates='newspaper')
+
+    def repr(self):
+        return f"<Newspaper {self.name}>"
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'logo': self.logo,
+            'link': self.link
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -20,3 +20,33 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    # Obtener los datos del cuerpo de la solicitud
+    request_body_user = request.get_json()
+
+    # Buscar el usuario en la base de datos
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({'message': "Usuario no encontrado"}), 404
+
+    # Actualizar los campos solo si están presentes en la solicitud
+    if "first_name" in request_body_user:
+        user.first_name = request_body_user["first_name"]
+    if "last_name" in request_body_user:
+        user.last_name = request_body_user["last_name"]
+    if "email" in request_body_user:
+        # Verificar si el nuevo email ya existe en otro usuario
+        existing_user = User.query.filter_by(email=request_body_user["email"]).first()
+        if existing_user and existing_user.id != user_id:
+            return jsonify({"error": "El correo ya está en uso por otro usuario"}), 400
+        user.email = request_body_user["email"]
+    if "password" in request_body_user:
+        user.password = request_body_user["password"]  # Recuerda encriptar la contraseña
+
+    # Guardar los cambios en la base de datos
+    db.session.commit()
+
+    return jsonify({'message': f'Usuario con id {user_id} ha sido actualizado correctamente'}), 200
